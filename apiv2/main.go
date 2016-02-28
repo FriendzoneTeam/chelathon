927d4ca0cd2fa083f181c0e8ebc315082a6de1ba
@HackeaMesta <rk521@hotmail.com>
2016-02-28 07:36:20 -0600

    kill the kira

--- apiv2/main.go
+++ apiv2/main.go
@@ -12,8 +12,11 @@ func main() {
 	
     r.POST("/parties", Party.AddParty)
     r.GET("/parties", Party.GetParties)
+    r.PUT("/parties",Party.UpdateParty);
+    
     r.POST("/venues", Venue.AddVenue)
     r.GET("/venues", Venue.GetVenues)
+
     // Persons
     r.POST("/persons", Person.AddPerson)
     r.GET("/persons", Person.GetPersons)
--- apiv2/routes/party/main.go
+++ apiv2/routes/party/main.go
@@ -5,39 +5,25 @@ import (
 	"strconv"
 	"github.com/gin-gonic/gin"
 	"chelathon/apiv2/models"
-	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
-	"encoding/json"
+	"upper.io/db.v2"
+	"upper.io/db.v2/mongo"
 )
+//Setting de la coneccion a mongo
+	var settings = mongo.ConnectionURL {
+		Address:  db.Host("ds049945.mongolab.com:49945"), // MongoDB hostname.
+		Database: "socialgopher",                         // Database name.
+		User:     "friendzonedb",                         // Optional user name.
+		Password: "friendzonedb",                         // Optional user password.
+	}
+
 
 func AddParty(c *gin.Context) {
 	
-
-	db, err := sql.Open("sqlite3", "./app.db")
+	sess, err := db.Open(mongo.Adapter, settings)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("db.Open(): %q\n", err)
 	}
-	defer db.Close()
-
-	sqlStmt := `
-	CREATE TABLE IF NOT EXISTS parties (id integer not null primary key, name text, location text, cover text, fecha text);
-	`
-
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
-	}
-
-	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-	stmt, err := tx.Prepare("insert into parties(name, location, cover, fecha) values(?, ?, ?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
+	defer sess.Close()
 
 	// Parametros del POST
 	name := c.PostForm("name")
@@ -48,69 +34,47 @@ func AddParty(c *gin.Context) {
 	latitude, _ := strconv.ParseInt(lat, 10, 64)
 	lng := c.PostForm("longitude")
 	longitude, _ := strconv.ParseInt(lng, 10, 64)
+
+	// Scheduler
+	partyCollection, err := sess.Collection("parties")
+	if err != nil {
+		log.Fatalf("Could not use collection: %q\n", err)
+	}
 	
 	// Nuestro nuevo registro 
 	reg := new(models.Party)
 	reg.Name = name
 	reg.Cover = cover
 	reg.Fecha = fecha
-	b, err := json.Marshal(models.Locations{Longitude: longitude, Latitude: latitude})
+	reg.Location = models.Locations{Longitude: longitude, Latitude: latitude}
 
-	_, err = stmt.Exec(name, string(b), cvr, fecha)
-	if err != nil {
-		log.Fatal(err)
-	}
-    tx.Commit()
-
+	partyCollection.Append(reg)
 	c.JSON(200, reg)
 }
 
 func GetParties(c *gin.Context) {
-	db, err := sql.Open("sqlite3", "./app.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	sqlStmt := `
-	CREATE TABLE IF NOT EXISTS parties (id integer not null primary key, name text, location text, cover text, fecha text);
-	`
-
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return
-	}
-
-	rows, err := db.Query("select id, name, location, cover, fecha from parties")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
 	parties := make([]models.Party, 0)
-	for rows.Next() {
-		var id int
-		var name string
-		var location string
-		var cover string
-		var fecha string
 
-		rows.Scan(&id, &name, &location, &cover, &fecha)
-		var locations models.Locations
-		err := json.Unmarshal([]byte(location), &locations)
-		if err != nil {
-	        panic(err)
-	    }
-		var reg models.Party
-		reg.Id = id
-		reg.Name = name
-		reg.Location = locations
-	    cvr, _ := strconv.ParseFloat(cover, 64)
-		reg.Cover = cvr
-		reg.Fecha = fecha
-		parties = append(parties, reg)
+	sess, err := db.Open(mongo.Adapter, settings)
+	if err != nil {
+		log.Fatalf("db.Open(): %q\n", err)
+	}
+
+	defer sess.Close()
+
+	partyCollection, err := sess.Collection("parties")
+	if err != nil {
+		log.Fatalf("sess.Collection(): %q\n", err)
+	}
+
+	err = partyCollection.Find().All(&parties)
+	if err != nil {
+		log.Fatalf("Find(): %q\n", err)
 	}
 
 	c.JSON(200, parties)
+}
+
+func UpdateParty(c *gin.Context){
+	c.Query
 }
