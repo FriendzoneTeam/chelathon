28d80b6c06db45021de80045daaf538b1c27cce7
Kira <kiramishima@outlook.com>
2016-02-28 05:21:12 -0600

    Cambio a SQLIte para chemas papu

--- apiv2/main.go
+++ apiv2/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	Party "chelathon/apiv2/routes/party"
 	Venue "chelathon/apiv2/routes/venue"
+	Person "chelathon/apiv2/routes/person"
 )
 
 func main() {
@@ -13,6 +14,8 @@ func main() {
     r.GET("/parties", Party.GetParties)
     r.POST("/venues", Venue.AddVenue)
     r.GET("/venues", Venue.GetVenues)
-    
+    // Persons
+    r.POST("/persons", Person.AddPerson)
+    r.GET("/persons", Person.GetPersons)
     r.Run()
 }
--- apiv2/models/main.go
+++ apiv2/models/main.go
@@ -1,25 +1,16 @@
 package models
 
-import (
-	"gopkg.in/mgo.v2/bson"
-)
-
 type Locations struct{
 	Longitude int64 `json:"longitude" bson:"longitude"`
 	Latitude int64 `json:"latitude" bson:"latitude"`
 }
 
 type Party struct{
-	Id bson.ObjectId `json:"id" bson:"_id"`
+	Id int `json:"id"`
 	Name string `json:"name" bson:"name"`
 	Location Locations `json:"locations" bson:"locations"`
 	Cover float64 `json:"cover" bson:"cover"`
 	Fecha string `json:"fecha" bson:"fecha"`
-}
-
-func (party *Party) SetID(id bson.ObjectId) error {
-	party.Id = id
-	return nil
 }
 
 type Venue struct {
@@ -32,18 +23,13 @@ type Venue struct {
 }
 
 type Person struct {
-	Id bson.ObjectId `json:"id" bson:"_id"`
+	Id int `json:"id"`
 	Nombre string `json:"nombre" bson:"nombre"`
 	Foto string `json:"foto" bson:"foto"`
 }
 
-func (person *Person) SetID(id bson.ObjectId) error {
-	person.Id = id
-	return nil
-}
-
 type Pool struct {
-	Id_user bson.ObjectId `json:"id_user" bson:"id_user"`
+	Id_user int `json:"id_user" bson:"id_user"`
 	Cuota	float64 `json:"cuota" bson:"cuota"`
-	Id_party bson.ObjectId `json:"id_party" bson:"id_party"`
+	Id_party int `json:"id_party" bson:"id_party"`
 }
