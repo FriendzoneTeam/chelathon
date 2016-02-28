e4b57ce9c010e0a167a9361ada7862ae0ca73570
@HackeaMesta <rk521@localhost.localdomain>
2016-02-28 01:21:25 -0600

    Agrega types

--- apiv2/models/main.go
+++ apiv2/models/main.go
@@ -9,4 +9,27 @@ type Party struct{
 	Id int
 	Name string
 	Location Locations
+	Cover float
+	fecha string
+}
+
+type Venue {
+	Name string
+	Location Locations
+	Dir1 string
+	Dir2 string
+	id_owner int
+	id_party int
+}
+
+type Person struct {
+	Id int
+	Nombre string
+	Foto string
+}
+
+type Pool struct {
+	Id_user int
+	Cuota	float
+	id_party int
 }
